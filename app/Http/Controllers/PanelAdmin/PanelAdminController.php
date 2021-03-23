1dce41bef62bc2f81c9bbb020fa9bdb47bd09e39
gdohinfotech <gdohinfotech@gmail.com>
2021-03-23 10:58:40 +0800

    Buat panel admin controller by MAGD

---
+++ app/Http/Controllers/PanelAdmin/PanelAdminController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\PanelAdmin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PanelAdminController extends Controller
+{
+    //
+}
