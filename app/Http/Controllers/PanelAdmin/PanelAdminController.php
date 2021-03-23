30a73b2a7ae14c1a3b01c2c80c9ba09b16b38b37
gdohinfotech <gdohinfotech@gmail.com>
2021-03-23 11:48:41 +0800

    layout panel admin menggunakan Admin LTE 3 by magd

--- app/Http/Controllers/PanelAdmin/PanelAdminController.php
+++ app/Http/Controllers/PanelAdmin/PanelAdminController.php
@@ -7,5 +7,8 @@ use App\Http\Controllers\Controller;
 
 class PanelAdminController extends Controller
 {
-    //
+    public function tampilDasbor()
+    {
+        return view('admin-dasbor.index');
+    }
 }
